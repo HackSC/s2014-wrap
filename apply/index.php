551bb146fa87e6d7615ac252d3422f3e565a653a
Iain Nash <info@codejoust.com>
2014-04-15 21:29:10 -0700

    fixing utf8 encoding, changing apply redirect

---
+++ apply/index.php
@@ -0,0 +1,32 @@
+<?php
+
+header('Location: https://docs.google.com/a/usc.edu/forms/d/11xTUTv4ULwXR7cwEDLLQKKC83loTy63s-4WpH8kHovg/viewform');
+
+?>
+<!doctype html>
+<html>
+<head>
+	<title>Apply to HackSC Fall 2014</title>
+	<style type="text/css">
+		body {
+			font-family:Helvetica;
+			min-height:100%;
+		}
+		iframe {
+			width:100%;
+			height: 95%;
+			display:block;
+		}
+
+	</style>
+</head>
+<body>
+	<h2>Apply to HackSC Fall 2014!</h2>
+	
+	<iframe src="https://docs.google.com/a/usc.edu/forms/d/11xTUTv4ULwXR7cwEDLLQKKC83loTy63s-4WpH8kHovg/viewform?embedded=true" width="760" height="900" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+	
+	<p>
+		<a href="https://docs.google.com/a/usc.edu/forms/d/11xTUTv4ULwXR7cwEDLLQKKC83loTy63s-4WpH8kHovg/viewform">(form above not loading?)</a>
+	</p>
+</body>
+</html>
